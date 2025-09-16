648436cf622b1274691f76cc4feace99b51823e9
affan <affanergul@outlook.com>
2025-09-16 21:24:07 +0300

    Update Test3.java

--- src/Project2/s/Shape/Test3.java
+++ src/Project2/s/Shape/Test3.java
@@ -2,6 +2,6 @@ package Project2.s.Shape;
 
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("test 2 olusturuldu");
+        System.out.println("test 2 degistirildi");
     }
 }
